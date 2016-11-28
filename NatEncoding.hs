ddc22610700a363ae8f9b85cff3c83d80ad6bd1f
Jonathan Prieto-Cubides <jprieto9@eafit.edu.co>
2016-11-28 18:47:40 -0500

    fixed some grammar

--- NatEncoding.hs
+++ NatEncoding.hs
@@ -1,20 +1,20 @@
 
--- | NatEncoding module
-
-{-# LANGUAGE UnicodeSyntax #-}
 
 {-|
 Module      : NatEncoding
 Description : alternative encoding for the natural numbers
-Copyright   : (c) Camilo Rodriguez and Jonathan Prieto-Cubide, 2016
+Copyright   : (c) Camilo Rodriguez and Jonathan Prieto-Cubides, 2016
 License     : GPL-3
-Maintainer  : Camilo Rodriguez and Jonathan Prieto-Cubide
+Maintainer  : Camilo Rodriguez and Jonathan Prieto-Cubides
 Stability   : stable
 Portability : POSIX
 
 NatEnconding is a Haskell program intends to provide an alternative
 enconding for the natural numbers.
 -}
+
+{-# LANGUAGE UnicodeSyntax #-}
+
 module NatEncoding
   where
 
@@ -26,8 +26,8 @@ import           Test.QuickCheck (Property, quickCheck, (==>))
 -- @Adapted from Lennart Augustsson's blog <https://goo.gl/3p6R0P>
 type Sym = String
 
-data Expr 
-  -- | The 'Var Sym' represents the normal form in ´v`|variable´'` 
+data Expr
+  -- | The 'Var Sym' represents the normal form in ´v`|variable´'`
   = Var Sym
   -- | The 'App Expr Expr' represents the normal form in ´(` λ-term λ-term ´)`
   | App Expr Expr
@@ -53,17 +53,17 @@ nf ee = spine ee []
     app ∷ Expr → [Expr] → Expr
     app f as    = foldl App f (map nf as)
 
--- |The 'betaEq' function will return if a couple λ-term are equal.
+-- |The 'betaEq' function asserts if two λ-terms are equal.
 betaEq ∷ Expr → Expr → Bool
 betaEq e1 e2 = alphaEq (nf e1) (nf e2)
 
--- |The 'freeVars' function finds the free var.
+-- |The 'freeVars' function finds the free variables of a λ-term.
 freeVars ∷ Expr → [Sym]
 freeVars (Var x)   = [x]
 freeVars (App f a) = freeVars f `union` freeVars a
 freeVars (Lam i e) = freeVars e \\ [i]
 
--- |The 'subst' function will replace all free occurrences of v by b inside x.
+-- |The 'subst' function replaces all free occurrences of v by b inside x.
 subst ∷ Sym → Expr → Expr → Expr
 subst v x = sub  -- subst v x b = sub b
   where
@@ -93,7 +93,7 @@ subst v x = sub  -- subst v x b = sub b
         vars ∷ [Sym]
         vars = fvx ++ freeVars e
 
--- |The 'substVar' function is a utility for replace one variable with another.
+-- |The 'substVar' function replaces one variable with another.
 substVar ∷ Sym → Sym → Expr → Expr
 substVar x s' = subst x (Var s')
 
@@ -104,29 +104,29 @@ alphaEq (App f a) (App f' a') = alphaEq f f' && alphaEq a a'
 alphaEq (Lam s e) (Lam s' e') = alphaEq e (substVar s' s e')
 alphaEq _ _                   = False
 
--- |The 'app2' function is for realize two applications in one λ-term.
+-- |The 'app2' function performs two applications in one λ-term.
 app2 ∷ Expr → Expr → Expr → Expr
 app2 f x = App $ App f x
 
--- |The 'app3' function is for realize three applications in one λ-term.
+-- |The 'app3' function peforms three applications.
 app3 ∷ Expr → Expr → Expr → Expr → Expr
 app3 f x y = App $ App (App f x) y
 
--- |The 'x¹, y¹, f¹' function that returns one variable.
+-- |The 'x¹, y¹, f¹' are handy variables.
 x¹, y¹, f¹ ∷ Expr
 x¹ = Var "x"
 y¹ = Var "y"
 f¹ = Var "f"
 
--- |The 'true' function that implements the combinator K.
+-- |The 'true' function is the combinator K.
 true ∷ Expr
 true = Lam "x" $ Lam "y" x¹
 
--- |The 'false' function that implements the combinator K*.  
+-- |The 'false' function is the combinator K*.
 false ∷ Expr
 false = Lam "x" $ Lam "y" y¹
 
--- |The 'false' function that implements the combinator if.
+-- |The 'false' function is the combinator if-then-else.
 iff ∷ Expr
 iff = Lam "f" $ Lam "x" $ Lam "y" $ app2 f¹ x¹ y¹
 
@@ -134,53 +134,53 @@ iff = Lam "f" $ Lam "x" $ Lam "y" $ app2 f¹ x¹ y¹
 testIf ∷ Expr → Expr → Expr → Expr
 testIf = app3 iff
 
--- |The 'pair' function that implements the combinator pair.
+-- |The 'pair' function is the combinator pair.
 pair ∷ Expr
 pair = Lam "x" $ Lam "y" $ Lam "f" $ app2 f¹ x¹ y¹
 
--- |The 'zero' function that implements the combinator I.
+-- |The 'zero' function is the combinator I.
 zero ∷ Expr
 zero = Lam "x" $ Var "x"
 
--- |The 'isZero' function that implements the combinator Zero.
+-- |The 'isZero' function is the combinator Zero.
 isZero ∷ Expr
 isZero = Lam "x" $ App x¹ true
 
--- |The 'testIf' function is for apply the combinator Zero.
+-- |The 'testIf' function applies the combinator Zero.
 tZero ∷ Expr → Expr
 tZero = App isZero
 
--- |The 'succ'' function that implements the combinator S+.
+-- |The 'succ'' function is the combinator S+.
 succ' ∷ Expr
 succ' = Lam "x" $ Lam "y" $ app2 y¹ false x¹
 
--- |The 'pred'' function that implements the combinator P-.
+-- |The 'pred'' function is the combinator P-.
 pred' ∷ Expr
 pred' = Lam "x" $ App x¹ false
 
--- |The 'yComb' function that implements the combinator Y.
+-- |The 'yComb' function is the combinator Y.
 yComb ∷ Expr
 yComb = Lam "f" $ App (Lam "x" fxx) (Lam "x" fxx)
   where
     fxx ∷ Expr
     fxx = App f¹ $ App x¹ x¹
 
--- |The 'applyY' function is for apply the combinator Y.
+-- |The 'applyY' function applies the combinator Y.
 applyY ∷ Expr → Expr
 applyY = App yComb
 
--- |The 'n¹, m¹, a¹' function that returns one variable.
+-- |The 'n¹, m¹, a¹' are handy variables.
 n¹, m¹, a¹ ∷ Expr
 n¹ = Var "n"
 m¹ = Var "m"
 a¹ = Var "a"
 
--- |The 'eN' function returns the λ-term associated with the natural number n
+-- |The 'eN' function returns the λ-term associated with the natural number n.
 eN ∷ Natural → Expr
 eN 0  = zero
 eN nn = app2 pair false $ eN (nn-1)
 
--- |The 'eAdd' function addition
+-- |The 'eAdd' function addition.
 eAdd ∷ Expr
 eAdd = applyY $ Lam "a" $ Lam "n" $ Lam "m" $
   testIf (tZero n¹) m¹ suma
@@ -189,7 +189,7 @@ eAdd = applyY $ Lam "a" $ Lam "n" $ Lam "m" $
       suma ∷ Expr
       suma = App succ' $ app2 a¹ (predW n¹) m¹
 
--- |The 'eMult' function multiplication
+-- |The 'eMult' performs the multiplication between two encoded naturals.
 eMult ∷ Expr
 eMult = applyY $ Lam "a" $ Lam "n" $ Lam "m" $
   testIf (tZero n¹) zero $ testIf (tZero m¹) zero multi
@@ -198,31 +198,35 @@ eMult = applyY $ Lam "a" $ Lam "n" $ Lam "m" $
       multi ∷ Expr
       multi = addW (app2 a¹ (predW n¹) m¹) m¹
 
--- |The 'predW' function is for apply predecessor
+
+-- The next functions were taken from
+-- http://www1.eafit.edu.co/asr/courses/ffpl-CM0848/lab.html
+
+-- |The 'predW' function is for apply predecessor.
 predW ∷ Expr → Expr
 predW = App pred'
 
--- |The 'predW' function is for apply addition
+-- |The 'predW' function applies addition.
 addW ∷ Expr → Expr → Expr
 addW = app2 eAdd
 
--- |The 'predW' function is for apply multiplication
+-- |The 'predW' function applies multiplication.
 multW ∷ Expr → Expr → Expr
 multW = app2 eMult
 
--- |The 'propPred' function test of predecessor
+-- |The 'propPred' function tests the predecessor function.
 propPred ∷ Natural → Property
 propPred n = n > 0 ==> betaEq (predW $ eN n) (eN $ n - 1)
 
--- |The 'propAdd' function test of addition
+-- |The 'propAdd' function tests  the addition function.
 propAdd ∷ Natural → Natural → Bool
 propAdd m n = betaEq (addW (eN m) (eN n)) (eN $ m + n)
 
--- |The 'propMult' function test of multiplication
+-- |The 'propMult' function tests the multiplication function.
 propMult ∷ Natural → Natural → Bool
 propMult m n = betaEq (multW (eN m) (eN n)) (eN $ m * n)
 
--- |The 'tests' is the function execute tests to valid operations of NatEncoding
+-- |The 'tests' function executes tests to valid operations of NatEncoding.
 tests ∷ IO ()
 tests = do
   quickCheck propPred
